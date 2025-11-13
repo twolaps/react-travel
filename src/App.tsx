33d796357541884a733f953c1ae642c5c269f351
edison <twolaps@163.com>
2025-11-13 13:50:58 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './App.module.css';
 import { Footer, Header, Carousel, SideMenu, ProductCollection, BusinessPartner} from './components';
 import {Row, Col, Typography} from 'antd';
-import { productList1, productList2, productList3 } from './MockUps';
+import { productList1, productList2, productList3 } from './pages/home/MockUps';
 import sideImage from './assets/images/sider_2019_12-09.png';
 import sideImage2 from './assets/images/sider_2019_02-04.png';
 import sideImage3 from'./assets/images/sider_2019_02-04-2.png';
