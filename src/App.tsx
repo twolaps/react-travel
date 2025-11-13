a9cac959bbd4930b1fa5b7cb9ba9363e66169654
edison <twolaps@163.com>
2025-11-13 14:05:48 +0800

    提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,8 @@
-import React from 'react';
-
-import styles from './App.module.css';
-import { Footer, Header, Carousel, SideMenu, ProductCollection, BusinessPartner} from './components';
-import {Row, Col, Typography} from 'antd';
-import { productList1, productList2, productList3 } from './pages/home/MockUps';
-import sideImage from './assets/images/sider_2019_12-09.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage3 from'./assets/images/sider_2019_02-04-2.png';
+import { Homepage } from './pages/home/Homepage';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <Header />
-      <div className={styles['page-content']}>
-        <Row style={{marginTop:20}}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-          />
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-          />
-
-        <ProductCollection
-          title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
-          sideImage={sideImage}
-          products={productList1}
-          />
-      </div>
-      <BusinessPartner/>
-      <Footer />
-    </div>
+    <Homepage/>
   );
 }
 
---
+++ src/pages/home/Homepage.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styles from './Homepage.module.css';
+import { Footer, Header, Carousel, SideMenu, ProductCollection, BusinessPartner} from '../../components';
+import {Row, Col, Typography} from 'antd';
+import { productList1, productList2, productList3 } from './MockUps';
+import sideImage from '../../assets/images/sider_2019_12-09.png';
+import sideImage2 from '../../assets/images/sider_2019_02-04.png';
+import sideImage3 from'../../assets/images/sider_2019_02-04-2.png';
+
+export class Homepage extends React.Component {
+    render(): React.ReactNode {
+        return (
+            <div className={styles.App}>
+                <Header />
+                <div className={styles['page-content']}>
+                    <Row style={{marginTop:20}}>
+                    <Col span={6}>
+                        <SideMenu />
+                    </Col>
+                    <Col span={18}>
+                        <Carousel />
+                    </Col>
+                    </Row>
+
+                    <ProductCollection
+                    title={<Typography.Title level={3} type="warning">爆款推荐</Typography.Title>}
+                    sideImage={sideImage}
+                    products={productList1}
+                    />
+
+                    <ProductCollection
+                    title={<Typography.Title level={3} type="danger">新品上市</Typography.Title>}
+                    sideImage={sideImage}
+                    products={productList1}
+                    />
+
+                    <ProductCollection
+                    title={<Typography.Title level={3} type="success">国内游推荐</Typography.Title>}
+                    sideImage={sideImage}
+                    products={productList1}
+                    />
+                </div>
+                <BusinessPartner/>
+                <Footer />
+            </div>
+        );
+    }
+}
